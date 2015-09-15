a59f12cb21f77c3bbb1a2b60c555d17c913b475a
caijinbei <caijinbei@caijinbei.jiuqi.com.cn>
2015-09-15 09:59:45 +0800

    1.0版本已完成

---
+++ jbeicAnnotationInject/src/com/jbeic/AnnotationInject/example/CBook.java
@@ -0,0 +1,23 @@
+package com.jbeic.AnnotationInject.example;
+
+/**
+ * TODO 简述           CBook      .<br>
+ *<p>TODO 详细描述</p>
+ *
+ * <p>Copyright: 版权所有 (c) JBeic</p>
+ *
+ * @author JBeic
+ * @version V1.0 2015年9月15日
+ */
+public class CBook implements Book {
+
+
+	public CBook(){
+		
+	}
+	@Override
+	public String toString() {
+		return "C BOOK";
+	}
+
+}
---
+++ jbeicAnnotationInject/src/com/jbeic/AnnotationInject/example/JavaBook.java
@@ -0,0 +1,22 @@
+package com.jbeic.AnnotationInject.example;
+
+/**
+ * TODO 简述      JavaBook 测试类           .<br>
+ *<p>TODO 详细描述</p>
+ *
+ * <p>Copyright: 版权所有 (c) JBeic</p>
+ *
+ * @author JBeic
+ * @version V1.0 2015年9月15日
+ */
+public class JavaBook implements Book {
+
+	public JavaBook(){
+		
+	}
+	@Override
+	public String toString() {
+		return "JAVA BOOK";
+	}
+
+}
